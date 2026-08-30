e2cd7702317f577ee7b0107e92839a3ee9b1c252
LeeTheY <peter20011148@gmail.com>
2026-08-30 19:27:53 +0900

    chore: upgrade apps in toss sdk and tds

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import aitDevtools from "@apps-in-toss/devtools/unplugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [aitDevtools.vite({ sdkVersion: "3" }), react()],
 });
